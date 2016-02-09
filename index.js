e4187d819cb081168024b742af2b2ac1f3c3785a
Johall <johnalexander.w.hall@gmail.com>
2016-02-08 21:25:03 -0500

    functioning handlebars app

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var expressHandlebars = require('express-handlebars');
+
+var PORT = process.env.PORT || 8090;
+
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+var icecreams = [ {name: 'vanilla', price: 10, awesomeness: 3},
+{name: 'chocolate', price: 4, awesomeness: 8},
+{name: 'banana', price: 1, awesomeness: 1},
+{name: 'greentea', price: 5, awesomeness: 7},
+{name: 'jawbreakers', price: 6, awesomeness: 2}, ];
+
+
+app.get("/vanilla", function(req, res){
+    res.render('home', icecreams[0]);
+});
+app.get("/chocolate", function(req, res){
+    res.render('home', icecreams[1]);
+});
+app.get("/banana", function(req, res){
+    res.render('home', icecreams[2]);
+});
+app.get("/greentea", function(req, res){
+    res.render('home', icecreams[3]);
+});
+app.get("/jawbreakers", function(req, res){
+    res.render('home', icecreams[4]);
+});
+
+app.listen(PORT, function() {
+  console.log("Listening on port %s", PORT);
+});
